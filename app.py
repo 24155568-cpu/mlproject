9037ee0c6c308144a7b4259a37202c7873398fad
Sakshi Mahapatra <24155568@kiit.ac.in>
2026-06-20 12:22:48 +0530

    initial commit

--- app.py
+++ app.py
@@ -4,16 +4,11 @@ import pandas as pd
 
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
-
 application = Flask(__name__)
 app = application
-
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
 @app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method == 'GET':
